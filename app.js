33bb249a6f948ba3f6e86ae7468aa3b0db6a0a18
aasthakumarii <aasthakumari.0105@gmail.com>
2026-04-21 00:31:01 +0530

    test full pipeline demo 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from final CI/CD Project for demo! 🚀');
+  res.send('Hello from final CI/CD Project for demo 2! 🚀');
 });
 
 app.get('/health', (req, res) => {
