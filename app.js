ba41ba51d92d5fcd8ec60cd2ea54146318cd4c56
aasthakumarii <aasthakumari.0105@gmail.com>
2026-04-21 00:19:37 +0530

    test full pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Project 🚀');
+  res.send('Hello from final CI/CD Project 🚀');
 });
 
 app.get('/health', (req, res) => {
@@ -19,5 +19,3 @@ app.listen(3000, () => {
 
 // app.js
 console.log("Testing dev branch");
-
-echo "Deployed via CI/CD 🚀"
